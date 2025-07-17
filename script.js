4b98c90deea2ee72def54ef64d4bc432df67afa7
charlielax23 <157481099+charlielax23@users.noreply.github.com>
2025-07-17 08:04:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+//Password Generator
+
+//Variable
+let input;
+let uppercase = document.getElementById("1")
+let lowercase = document.getElementById("2")
+let symbols = document.getElementById("3")
+let numbers = document.getElementById("4")
+let btn = document.getElementById("btn")
+let result = document.getElementById("result")
+
+//BTN
+btn.onclick = function(){
+
+    //Variable
+    input = document.getElementById("input").value
+    input = Number(input)
+
+    //Variable
+    let upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+    let lower = ["abcdefghijklmnopqrstuvwxyz"]
+    let symbol = ["!@#$%^&*()-_=+[]{};:,.<>/?"]
+    let number = ["0123456789"]
+
+    //Variable
+    let all_char = []
+    let password = []
+
+    //If
+    if(input == ""){
+        result.textContent = "Enter Number"
+        return
+    }
+
+    else if(input > 30){
+        result.textContent = "The maximun is 30"
+        return
+    }
+    else if(input <= 0){
+        result.textContent = "Enter Positive Number"
+        return
+    }
+    else if(isNaN(input)){
+        result.textContent = "Invalid. Try Again"
+        return
+    }
+    else if(!uppercase.checked && !lowercase.checked && !symbols.checked && !numbers.checked){
+        result.textContent = "Check Some Boxes"
+        return
+    }
+    else{
+
+        if(uppercase.checked){
+            all_char += upper
+        }
+        if(lowercase.checked){
+            all_char += lower
+        }
+        if(symbols.checked){
+            all_char += symbol
+        }
+        if(numbers.checked){
+            all_char += number
+        }
+    }
+
+    //For
+    for(let i = 0; i < input; i++){
+
+        //Random
+        let random = Math.floor(Math.random() * all_char.length)
+
+        //Result
+        password += all_char[random]
+    }
+
+    //Result
+    result.textContent = password
+}
